02b615cd30ffc01e8e2c185cf68af48d45efde5d
funyushumpei <83898529+funyushumpei@users.noreply.github.com>
2021-07-06 11:33:53 +0900

    Add files via upload

---
+++ GachaGacha/gs_user_table.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2021 年 7 朁E06 日 04:31
+-- サーバのバージョン： 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_user_table`
+--
+
+CREATE TABLE IF NOT EXISTS `gs_user_table` (
+`id` int(12) NOT NULL,
+  `user_name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_pw` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `life_flag` int(1) NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_user_table`
+--
+
+INSERT INTO `gs_user_table` (`id`, `user_name`, `user_id`, `user_pw`, `life_flag`, `datetime`) VALUES
+(1, 'user001', 'user001', 'user001', 0, '2021-06-16 11:44:43'),
+(2, 'test002', 'test002', 'test002', 0, '2021-06-22 17:48:56');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
